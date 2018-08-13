8288f08cef9f892a3109783d0a143e20dba28741
mikob <fxrock2002@yahoo.com>
2018-08-13 17:57:20 +0900

    Fixed issue with setting inactivity auto off mins to 0

--- chrome-extension/src/components/options-page.vue
+++ chrome-extension/src/components/options-page.vue
@@ -56,7 +56,7 @@
             <label class="w-note">
                 <i class="icon timer-off"></i>
 				<div>
-					Automatically shut off &nbsp;&nbsp;<input class="right" style="width: 3.5em" v-model="optionsPageStore.inactivityAutoOffMins" type="number" min="0" max="525600" /> &nbsp;&nbsp;minutes after the last valid command.
+					Automatically shut off &nbsp;&nbsp;<input class="right" style="width: 3.5em" v-model.number="optionsPageStore.inactivityAutoOffMins" type="number" min="0" max="525600" /> &nbsp;&nbsp;minutes after the last valid command.
 					<div class="mute">Set to 0 to never automatically shut off.</div>
 				</div>
             </label>
