bfaf8bfd8411625677b66d4704194124fce27c1e
mikob <fxrock2002@yahoo.com>
2018-09-12 15:42:13 +0900

    Added top sites plugin

---
+++ src/TopSites/TopSites.ts
@@ -0,0 +1,126 @@
+
+/*
+ * LipSurf plugin for going to popular sites 
+ */
+/// <reference path="../@types/plugin-interface.d.ts"/>
+
+ namespace TopSitesPlugin {
+     declare const PluginBase: IPlugin;
+
+     export let Plugin = Object.assign({}, PluginBase, {
+        niceName: 'Top Sites',
+        description: 'Commands for going to the most popular websites',
+        match: /.*/,
+        homophones: {
+
+        },
+        authors: "Miko",
+
+        commands: [
+            {
+                name: 'Youtube',
+                global: true,
+                match: 'youtube',
+                pageFn: async function() {
+                    window.location.href = 'https://www.youtube.com/';
+                }
+            },
+            {
+                name: 'Wikipedia',
+                global: true,
+                match: 'wikipedia',
+                pageFn: async function() {
+                    window.location.href = 'https://www.wikipedia.org/';
+                }
+            },
+            {
+                name: 'Facebook',
+                global: true,
+                match: 'facebook',
+                pageFn: async function() {
+                    window.location.href = 'https://www.facebook.com/';
+                }
+            },
+            {
+                name: 'Twitter',
+                global: true,
+                match: 'twitter',
+                pageFn: async function() {
+                    window.location.href = 'https://twitter.com/';
+                }
+            },
+            {
+                name: 'Instagram',
+                global: true,
+                match: 'instagram',
+                pageFn: async function() {
+                    window.location.href = 'https://www.instagram.com/';
+                }
+            },
+            {
+                name: 'Amazon',
+                global: true,
+                match: 'amazon',
+                pageFn: async function() {
+                    window.location.href = 'https://www.amazon.com/';
+                }
+            },
+            {
+                name: 'Ebay',
+                global: true,
+                match: 'ebay',
+                pageFn: async function() {
+                    window.location.href = 'https://www.ebay.com/';
+                }
+            },
+            {
+                name: 'VK',
+                global: true,
+                match: 'vk',
+                pageFn: async function() {
+                    window.location.href = 'https://vk.com';
+                }
+            },
+            {
+                name: 'Netflix',
+                global: true,
+                match: 'netflix',
+                pageFn: async function() {
+                    window.location.href = 'https://www.netflix.com';
+                }
+            },
+            {
+                name: 'Twitch',
+                global: true,
+                match: 'twitch',
+                pageFn: async function() {
+                    window.location.href = 'https://twitch.tv';
+                }
+            },
+            {
+                name: 'New York Times',
+                global: true,
+                match: 'new york times',
+                pageFn: async function() {
+                    window.location.href = 'https://www.nytimes.com';
+                }
+            },
+            {
+                name: 'Github',
+                global: true,
+                match: 'github',
+                pageFn: async function() {
+                    window.location.href = 'https://github.com/';
+                }
+            },
+            {
+                name: 'Hacker News',
+                global: true,
+                match: ['hacker news', 'y combinator'],
+                pageFn: async function() {
+                    window.location.href = 'https://news.ycombinator.com/';
+                }
+            },
+        ]
+     })
+ }
