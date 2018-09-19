83178c9a8962ac4e4ab05c9ff65d5c7b64c81cf4
mikob <fxrock2002@yahoo.com>
2018-09-19 23:06:36 +0900

    Move vote/comments/click homophones to general english lang recg. Added HN plugin.

---
+++ src/HackerNews/HackerNews.ts
@@ -0,0 +1,62 @@
+/// <reference path="../@types/plugin-interface.d.ts"/>
+
+namespace HackerNewsPlugin {
+    declare const PluginBase: IPlugin;
+
+    interface IHackerNewsPlugin extends IPlugin {
+        getThingAtIndex: (index: number) => HTMLElement;
+    }
+
+    export let Plugin = Object.assign({}, PluginBase, {
+        niceName: 'Hacker News',
+        description: 'Basic controls for news.ycombinator.com.',
+        version: '1.0.0',
+        apiVersion: '1',
+        match: /^https?:\/\/news.ycombinator.com/,
+        homophones: {
+            'floor': 'more',
+            '4': 'more',
+        },
+        authors: "Miko",
+
+        getThingAtIndex: (index: number) => {
+            return $(`table.itemlist tr td .rank:contains("${index}")`).closest('tr.athing');
+        },
+
+        commands: [
+            {
+                name: 'Upvote',
+                description: "Upvote a post",
+                match: 'upvote #',
+                pageFn: async (index:number) => {
+                    Plugin.getThingAtIndex(index).find('.votearrow[title="upvote"]').get(0).click();
+                }
+            },
+            {
+                name: 'Visit Comments',
+                description: "See the comments for a given post",
+                match: ['comments #', 'discuss #'],
+                pageFn: async (index:number) => {
+                    Plugin.getThingAtIndex(index).find('+tr .subtext a[href^="item?id="]').get(0).click();
+                }
+            },
+            {
+                name: 'Visit Post',
+                description: "Click a post",
+                match: ['click #', 'visit #'],
+                pageFn: async (index:number) => {
+                    Plugin.getThingAtIndex(index).find('a.storylink').get(0).click();
+                }
+            },
+            {
+                name: 'Next Page',
+                description: "Show more hacker news items",
+                match: ['next page', 'show more', 'more'],
+                pageFn: async (index:number) => {
+                    $('a.morelink').get(0).click();
+                }
+            },
+
+        ],
+    });
+}
