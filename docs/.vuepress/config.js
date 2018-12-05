cba2f5360cbe810c2950563504b110f3e170760a
mikob <fxrock2002@yahoo.com>
2018-12-05 13:40:52 +0900

    Edit on github link added to docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,8 @@ module.exports = {
 		logo: '/icon-128.png',
 		repo: 'lipsurf/plugins',
 		footer: '© 2018 LipSurf',
+		editLinks: true,
+		editLinkText: 'Improve this page',
 		algolia: {
 		    apiKey: '813bcf2a81a839cbea24b7899885d1b6',
 		    indexName: 'lipsurf'
