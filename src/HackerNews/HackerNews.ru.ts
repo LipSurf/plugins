9b73721e59cf9d03ce73e5adf912e99cac3d47c6
ulalasava <43239464+ulalasava@users.noreply.github.com>
2018-12-08 18:18:57 +0900

    Create HackerNews.ru.ts

---
+++ src/HackerNews/HackerNews.ru.ts
@@ -0,0 +1,44 @@
+/// <reference path="../@types/plugin-interface.d.ts"/>
+import { PluginBase } from '../PluginBase';
+
+    export let Plugin: IHackerNewsPlugin = Object.assign({}, PluginBase, {
+        niceName: 'Hacker News',
+        description: 'Basic controls for news.ycombinator.com.',
+        authors: "Hanna",
+        commands: [
+            {
+                name: 'Upvote',
+                description: "Upvote a post",
+                match: 'upvote #',
+                pageFn: async (transcript:string, index:number) => {
+                    Plugin.getThingAtIndex(index).find('.votearrow[title="upvote"]').get(0).click();
+                }
+            },
+            {
+                name: 'Visit Comments',
+                description: "See the comments for a given post",
+                match: ['comments #', 'discuss #'],
+                pageFn: async (transcript:string, index:number) => {
+                    Plugin.getThingAtIndex(index).find('+tr .subtext a[href^="item?id="]').get(0).click();
+                }
+            },
+            {
+                name: 'Visit Post',
+                description: "Click a post",
+                match: ['click #', 'visit #'],
+                pageFn: async (transcript:string, index:number) => {
+                    Plugin.getThingAtIndex(index).find('a.storylink').get(0).click();
+                }
+            },
+            {
+                name: 'Next Page',
+                description: "Show more hacker news items",
+                match: ['next page', 'show more', 'more'],
+                pageFn: async (transcript:string, index:number) => {
+                    $('a.morelink').get(0).click();
+                }
+            },
+
+        ],
+    });
+}
