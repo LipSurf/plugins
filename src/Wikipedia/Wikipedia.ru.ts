000e7db31dcc8011301df2a813e19bcd606d4a2e
ulalasava <43239464+ulalasava@users.noreply.github.com>
2018-12-27 19:24:49 +0900

    Update Wikipedia.ru.ts

--- src/Wikipedia/Wikipedia.ru.ts
+++ src/Wikipedia/Wikipedia.ru.ts
@@ -1,24 +1,14 @@
-export let Plugin = Object.assign({}, PluginBase, {
+
     niceName: 'Wikipedia',
-    description: 'The Wikipedia search engine.',
-    version: '1.0.1',
-    apiVersion: '1',
-    match: /.*/,
-    homophones: {
-      'wiki': 'wikipedia',
-    },
-    authors: 'Aparajita Fishman',
+    description: 'Поиск по Википедии.',
+    authors: 'Hanna',
 
     commands: [{
-      name: 'Wikipedia',
-      description: "Do a wikipedia search.",
-      global: true,
-      match: 'wikipedia *',
-      fn: async (transcript: string, searchQuery: string) => {
-        chrome.tabs.create({
-          url: `https://wikipedia.org/w/index.php?search=${encodeURIComponent(searchQuery).replace(/%20/g, '+')}`,
-          active: true
-        });
+      "Wikipedia": {
+      name: 'Википедия',
+      description: "Открывает сайт Википедия.",
+      match: ['википедия *'],
+    
       }
     }
     ],
