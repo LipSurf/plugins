c3443855cecf895a93f9916d80879e43a5d43496
ulalasava <43239464+ulalasava@users.noreply.github.com>
2018-12-27 19:51:08 +0900

    Create AntiProcrastination.ru.ts

---
+++ src/AntiProcrastination/AntiProcrastination.ru.ts
@@ -0,0 +1,60 @@
+/// <reference path="../@types/plugin-interface.d.ts"/>
+import { PluginBase } from '../PluginBase';
+
+export module AntiProcrastinationPlugin {
+    interface IAntiProcrastinationPlugin extends IPlugin {
+        OPEN_X_FOR_Y_TIME_REGX: RegExp;
+        OPEN_REGX: RegExp;
+    }
+
+    export let Plugin: IAntiProcrastinationPlugin = Object.assign({}, PluginBase, {
+        niceName: 'Anti-procrastination',
+        description: 'Tools for curbing procrastination.',
+        match: /.*/,
+        author: 'Miko',
+        OPEN_X_FOR_Y_TIME_REGX: /^open (.*) for (\d+) (seconds|minutes?|hours?)$/,
+        OPEN_REGX: /^open\b/,
+        commands: [
+            {
+                name: 'Self Destructing Tab',
+                description: 'Open a new tab with x website for y time. Useful for limiting the time-sucking power of sites like facebook, reddit, twitter etc.',
+                global: true,
+                match: {
+                    description: 'Say "open [website name] for x seconds/minutes/hours"',
+                    fn: (transcript: string) => {
+                        let fullMatch = transcript.match(Plugin.OPEN_X_FOR_Y_TIME_REGX);
+                        if (fullMatch) {
+                            return fullMatch;
+                        } else if (Plugin.OPEN_REGX.test(transcript)) {
+                            // ideally it would be smarter than just testing (open) but that functionality 
+                            // should be built into the recognizer
+                            return false;
+                        }
+                    }
+                },
+                // delay is needed to get more accurate site name
+                delay: 600,
+                fn: async (transcript: string, fullMatch: string, siteStr: string, secondsStr: string, unit: string) => {
+                    let seconds = Number(secondsStr);
+                    if (unit.startsWith('minute'))
+                        seconds *= 60;
+                    else if (unit.startsWith('hour'))
+                        seconds *= 3600;
+                    if (~siteStr.indexOf('hacker news'))
+                        siteStr = 'news.ycombinator';
+                    else if (siteStr === 'reddit')
+                        // faster than the redirect
+                        siteStr = 'old.reddit.com'
+                    let site = `https://${siteStr.replace(/\s+/g, '').replace("'", '').replace('.com', '').replace('dot com', '')}.com`;
+                    let id = chrome.tabs.create({
+                        url: site,
+                        active: true,
+                    }, (tab) => {
+                        setTimeout(() => {
+                            chrome.tabs.remove(tab.id);
+                        }, seconds * 1000);
+                    });
+                }
+            }],
+    });
+}
