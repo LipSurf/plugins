53a67875667a2d611460d34c14c50acc97b91471
ulalasava <43239464+ulalasava@users.noreply.github.com>
2019-01-01 21:02:29 +0900

    Update HackerNews.ru.ts

--- src/HackerNews/HackerNews.ru.ts
+++ src/HackerNews/HackerNews.ru.ts
@@ -1,40 +1,28 @@
 /// <reference path="../@types/plugin-interface.d.ts"/>
-import { PluginBase } from '../PluginBase';
+import { HackerNewsPlugin } from './HackerNews';
 
-    export let Plugin: IHackerNewsPlugin = Object.assign({}, PluginBase, {
         niceName: 'Hacker News',
         description: 'Basic controls for news.ycombinator.com.',
         authors: "Hanna",
-        commands: [
-            {
-                command: 'Upvote',
+        commands: {
+             'Upvote': {
                 name: 'Голосовать за',
                 description: "Голосует за пост названного номера",
                 match: ['голосовать за #'],
-                }
-            },
-            {
-                command: 'Visit Comments',
+                },
+              'Visit Comments': {
                 name: 'Открыть комментарии',
                 description: "Открывает комментарии к выбранному посту",
                 match: ['комментарии #'],
-                }
-            },
-            {
-                command: 'Visit Post',
+                },
+              'Visit Post': {
                 name: 'Открыть пост',
                 description: "Кликает на пост названного номера",
                 match: ['кликнуть #', 'открыть #'],
-                }
-            },
-            {
-                command: 'Next Page',
+                },
+              'Next Page': {
                 name: 'Следующая страница',
                 description: "Делает видимыми следующие посты",
                 match: ['следующая страница', 'больше'],
-                }
-            },
-
-        ],
-    });
-}
+                },
+    };
