cea6c0c038dab722a6d7dc77cba401420b981006
mikob <fxrock2002@yahoo.com>
2019-01-02 00:30:32 +0900

    Added russian timer

---
+++ src/Timer/Timer.ru.ts
@@ -0,0 +1,36 @@
+/// <reference path="../@types/plugin-interface.d.ts"/>
+import { TimerPlugin } from './Timer';
+
+TimerPlugin.Plugin.languages.ru = {
+    niceName: "Таймер",
+    description: "Поиск в Google",
+    authors: "Hanna",
+    homophones: {
+    },
+    commands: {
+        "Set Timer": {
+            name: 'Установить таймер',
+            description: 'Показывает уведомление и говорит "Таймер установлен" после того, как назван промежуток времени.',
+            match: {
+                // does not handle decimals
+                description: 'Скажите "Установить таймер [имя таймера (не обязательно)] на x секунд/минут/часов"',
+                fn: (transcript: string) => {
+                    let regxMatch = transcript.match(/^установить (?:(.*) )?таймер (?:на )?(полчаса|полтора часа|(\d+) ?(секунд(?:у|ы)?|минут(?:у|ы)?|час(?:а|ов)?)(?:(?: и)? (?:(?:(\d+) (секунд(?:у|ы)?|минут(?:у|ы)?))))?)/);
+                    if (regxMatch) {
+                        let quarter = null;
+                        let timerName = regxMatch[1];
+                        let half = regxMatch[2] && regxMatch[2].startsWith('пол') ? 'half' : null;
+                        let quantity = regxMatch[3];
+                        let unit = regxMatch[4] ? regxMatch[4].startsWith('секунд') ? 'second' : regxMatch[4].startsWith('минут') ? 'minute' : 'hour' : '';
+                        let quantity2 = regxMatch[5];
+                        let unit2 = regxMatch[6] ? regxMatch[6].startsWith('секунд') ? 'second' : regxMatch[6].startsWith('минут') ? 'minute' : 'hour' : '';
+                        return [regxMatch[0], timerName, quantity, unit, quantity2, unit2, half, quarter];
+                    } else if (/^установить (?:(.*) )?(?:таймер)? (?:на )?/.test(transcript)) {
+                        // ideally it would be smarter. Smartness should be built into the recognizer
+                        return false;
+                    }
+                }
+            },
+        },
+    }
+};
