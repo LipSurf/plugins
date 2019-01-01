007fa3e0c6b9926fe573699575e8c6d45bf99297
ulalasava <43239464+ulalasava@users.noreply.github.com>
2019-01-01 22:06:36 +0900

    Update Wikipedia.ru.ts

--- src/Wikipedia/Wikipedia.ru.ts
+++ src/Wikipedia/Wikipedia.ru.ts
@@ -1,16 +1,18 @@
+/// <reference path="../@types/plugin-interface.d.ts"/>
+import { WikipediaPlugin } from './Wikipedia';
 
+WikipediaPlugin.Plugin.languages.ru = {
     niceName: 'Wikipedia',
     description: 'Поиск по Википедии.',
     authors: 'Hanna',
-
+    homophones: {
+        'википедия': 'wikipedia',
+    },
     commands: [{
       "Wikipedia": {
-      name: 'Википедия',
-      description: "Открывает сайт Википедия.",
-      match: ['википедия *'],
-    
+          name: 'Википедия',
+          description: "Выполняет поиск по википедии. Скажите википедия [запрос].",
+          match: ['википедия *',],
       }
-    }
-    ],
-  });
-}
+    }],
+};
