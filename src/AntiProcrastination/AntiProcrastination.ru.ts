4e3a9346e444df3b3e5b0615eedb1fd10ca42987
mikob <fxrock2002@yahoo.com>
2019-01-03 18:29:35 +0900

    Disabling russian weather for now. Fixed anti-procrastination

--- src/AntiProcrastination/AntiProcrastination.ru.ts
+++ src/AntiProcrastination/AntiProcrastination.ru.ts
@@ -1,49 +1,29 @@
 /// <reference path="../@types/plugin-interface.d.ts"/>
-import { AntiProcrastinationPlugin } from './AntiProcrastinationPlugin';
+import { AntiProcrastinationPlugin } from './AntiProcrastination';
 
 AntiProcrastinationPlugin.Plugin.languages.ru = {
-        niceName: 'Анти-прокрастинатор',
-        authors: 'Hanna',
-        commands: {
-              'Self Destructing Tab',
-                name: 'Самозакрывающаяся вкладка',
-                description: 'Открывает новую вкладку только на заданное время. Удобно для ограничения пользования сайтами-времяубийцами вроде facebook, reddit, twitter etc.',
-                match: {
-                    description: 'Скажите "открыть [название сайта] на x секунд/минут/часов"',
-                    fn: (transcript: string) => {
-                        let fullMatch = transcript.match(Plugin.OPEN_X_FOR_Y_TIME_REGX);
-                        if (fullMatch) {
-                            return fullMatch;
-                        } else if (Plugin.OPEN_REGX.test(transcript)) {
-                            // ideally it would be smarter than just testing (open) but that functionality 
-                            // should be built into the recognizer
-                            return false;
-                        }
+    niceName: 'Анти-прокрастинатор',
+    authors: 'Hanna',
+    commands: {
+        'Self Destructing Tab': {
+            name: 'Самозакрывающаяся вкладка',
+            description: 'Открывает новую вкладку только на заданное время. Удобно для ограничения пользования сайтами-времяубийцами вроде facebook, reddit, twitter etc.',
+            match: {
+                description: 'Скажите "открыть [название сайта] на x секунд/минут/часов"',
+                fn: (transcript: string) => {
+                    let fullMatch = transcript.match(/^открыть (.*) на (\d+) (секунд(?:у|ы)?|минут(?:у|ы)?|час(?:а|ов)?)/);
+                    if (fullMatch) {
+                        fullMatch[3] = fullMatch[3].startsWith('минут') ? 'minute' : fullMatch[3].startsWith('час') ? 'hour' : 'second';
+                        return fullMatch;
+                    } else if (/^открыть\b/.test(transcript)) {
+                        // ideally it would be smarter than just testing (open) but that functionality 
+                        // should be built into the recognizer
+                        return false;
                     }
-                },
-                // delay is needed to get more accurate site name
-                delay: 600,
-                fn: async (transcript: string, fullMatch: string, siteStr: string, secondsStr: string, unit: string) => {
-                    let seconds = Number(secondsStr);
-                    if (unit.startsWith('minute'))
-                        seconds *= 60;
-                    else if (unit.startsWith('hour'))
-                        seconds *= 3600;
-                    if (~siteStr.indexOf('hacker news'))
-                        siteStr = 'news.ycombinator';
-                    else if (siteStr === 'reddit')
-                        // faster than the redirect
-                        siteStr = 'old.reddit.com'
-                    let site = `https://${siteStr.replace(/\s+/g, '').replace("'", '').replace('.com', '').replace('dot com', '')}.com`;
-                    let id = chrome.tabs.create({
-                        url: site,
-                        active: true,
-                    }, (tab) => {
-                        setTimeout(() => {
-                            chrome.tabs.remove(tab.id);
-                        }, seconds * 1000);
-                    });
                 }
-            }],
-    });
+            },
+            // delay is needed to get more accurate site name
+            delay: 600,
+        },
+    }
 }
