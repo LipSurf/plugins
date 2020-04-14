044a2e84e679407d12ec3dccbbc4686289f9091f
mikob <fxrock2002@yahoo.com>
2020-04-14 00:00:09 -0500

    Added French (for Duolingo)

---
+++ src/Scroll/Scroll.fr.ts
@@ -0,0 +1,17 @@
+/// <reference types="lipsurf-plugin-types"/>
+import ScrollPlugin from "./Scroll";
+
+ScrollPlugin.languages!.fr = {
+    niceName: "Desplazarse",
+    authors: "Miko",
+    commands: {
+        "Scroll Help Down": {
+            name: "Faire Défiler l'aide",
+            match: "faire défiler l'aide",
+        },
+        "Scroll Help Up": {
+            name: "Faire Défiler l'aide Vers le Haut",
+            match: "faire défiler l'aide vers le haut",
+        },
+    },
+};
