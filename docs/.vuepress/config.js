68fdf590e84eddeced4f35d646b80da7263e455f
Ahmed Kamal <en.ahmed.mk@gmail.com>
2020-06-30 00:49:16 +0200

    add placeholder to the search bar on the docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,7 @@ module.exports = {
 		editLinks: true,
 		editLinkText: 'Improve this page',
 		docsDir: 'docs',
+		searchPlaceholder: 'Search...',
 		algolia: {
 		    apiKey: '813bcf2a81a839cbea24b7899885d1b6',
 		    indexName: 'lipsurf'
