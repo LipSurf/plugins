7be9c98b274e70c32a0807be767123ece99d52e1
mikob <fxrock2002@yahoo.com>
2020-07-04 15:14:55 -0500

    More Japanese translations from Hiroki. Needs testing. Need to finish dynamic match commands - such as in Gmail.

--- src/Google/Google.ja.ts
+++ src/Google/Google.ja.ts
@@ -2,17 +2,33 @@
 import Google from './Google';
 
 Google.languages!.ja = {
-    niceName: "ググる",
-    description: "Googleで検索します",
-    authors: "Miko",
+    niceName: "Google検索",
+    description: "Googleで検索",
+    authors: "Miko, Hiroki Yamazaki",
     homophones: {
-        'google': 'ぐぐる'
+        'ぐーぐる': 'ぐぐる',
+        'google': 'ぐぐる',
     },
     commands: {
         "Search": {
-            name: "検索します",
-            description: "ググる「空欄を埋めて下さい」",
-            match: "ぐぐる*",
+            name: "検索",
+            description: "検索するテキスト(*)を組み合わせてください",
+            match: ["けんさく*", "ぐぐる*"],
+        },  
+        "I'm Feeling Lucky": {
+            name: "I'm Feeling Lucky",
+            description: 'Google検索で"I\'m Feeling Lucky"をクリックしたときと同じ動作をします。検索ワードの検索結果でGoogleが正しいと判断した場合に一番最初のページに移動します。',
+            match: ["らっきー*"]
+        },
+        "Google Calendar": {
+            name: "Googleカレンダー",
+            description: "Googleカレンダーを開きます",
+            match: ["かれんだー", "ぐぐるかれんだー"]
+        },
+        "Add Event to Google Calendar": {
+            name: "Googleカレンダーに予定を追加",
+            description: "Googleカレンダーに予定を追加します",
+            match: ["ぐぐるかれんだーによていをついか", "ぐぐるかれんだーについか"]
         }
     }
 };
