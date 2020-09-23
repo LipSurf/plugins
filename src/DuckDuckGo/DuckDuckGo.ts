41187dbb13760577f06b8f1696633efde67cd5fc
mikob <fxrock2002@yahoo.com>
2020-09-23 18:15:32 -0500

    Upped to v3.9.0

--- src/DuckDuckGo/DuckDuckGo.ts
+++ src/DuckDuckGo/DuckDuckGo.ts
@@ -7,7 +7,7 @@ declare const PluginBase: IPluginBase;
 export default <IPluginBase & IPlugin> {...PluginBase, ...{
     niceName: 'DuckDuckGo',
     description: 'The duckduckgo search engine.',
-    version: '3.8.0',
+    version: '3.9.0',
     match: /.*/,
     homophones: {
         'search': 'duck',
