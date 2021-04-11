5cb310996b460934cdc027f1c0de6631da4eb999
mikob <mikojunky@fastmail.com>
2021-04-11 11:24:47 -0500

    Started Russian keyboard translations.

---
+++ src/Keyboard/Keyboard.ru.ts
@@ -0,0 +1,13 @@
+/// <reference types="lipsurf-types/extension"/>
+import Keyboard from "./Keyboard";
+
+Keyboard.languages!.ru = {
+  niceName: "Клавиатура",
+  authors: "Miko",
+  commands: {
+    "Press Enter": {
+      name: "нажмите ввод",
+      match: "ввод",
+    },
+  },
+};
