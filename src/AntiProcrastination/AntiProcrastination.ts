8267d3426c729f1e6b0aec14242974b2c61b8222
mikob <492864+mikob@users.noreply.github.com>
2022-01-11 22:13:48 -0600

    Publish
     - @lipsurf/common@2.1.3
     - @lipsurf/extension@4.6.0
     - lipsurf.com@0.2.3
     - @lipsurf/plugins@4.4.0
     - @lipsurf/premium-plugins@4.6.0

--- src/AntiProcrastination/AntiProcrastination.ts
+++ src/AntiProcrastination/AntiProcrastination.ts
@@ -11,7 +11,7 @@ export default <IPluginBase & IPlugin>{
     niceName: "Anti-procrastination",
     description: "Tools for curbing procrastination.",
     match: /.*/,
-    version: "4.4.0-alpha.0",
+    version: "4.4.0",
     apiVersion: 2,
     authors: "Miko",
     commands: [
